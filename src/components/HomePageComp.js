e58413796fcb3aa763a717e9c613afdbe17b5b4e
Saumya Rawal <114496132+technoprogramming92@users.noreply.github.com>
2024-12-26 14:45:15 +0530

    homepage-content-change

---
+++ src/components/HomePageComp.js
@@ -0,0 +1,97 @@
+import React, { useRef } from "react";
+import { motion, useScroll } from "framer-motion";
+import Liicon from "./Liicon";
+
+const Details = ({ type, info }) => {
+  const ref = useRef(null);
+  return (
+    <li
+      ref={ref}
+      className="my-8 first:mt-0 last:mb-0 ml-6 flex flex-col items-center justify-between md:w-[80%]"
+    >
+      <Liicon reference={ref} />
+      <motion.div
+        initial={{ y: 50 }}
+        whileInView={{ y: 0 }}
+        transition={{ duration: 0.5, type: "spring" }}
+      >
+        <h3 className="capitalize font-bold text-2xl sm:text-xl xs:text-lg">
+          {type}
+        </h3>
+        {/* <span className="capitalize font-medium text-[#b6b1b1] xs:text-sm sm:ml-1">
+          {time} | {place}
+        </span> */}
+        <p className="font-medium w-full md:text-sm sm:ml-1 text-[20px]">
+          {info}
+        </p>
+      </motion.div>
+    </li>
+  );
+};
+function HomePageComp() {
+  const ref = useRef(null);
+  const { scrollYProgress } = useScroll({
+    target: ref,
+    offset: ["start end", "center start"],
+  });
+  return (
+    <div className="my-64">
+      <h2 className="font-bold text-5xl mb-32 w-full text-center md:text-6xl xs:text-4xl md:mb-16">
+        Everything You Need for Hassle-Free Tax Filing
+      </h2>
+
+      <p className="w-[75%] mx-auto relative mt-12 font-medium md:text-base sm:text-sm xs:text-xs text-2xl text-justify">
+        Stress-Free Tax Services, From Start to Finish When it comes to taxes, I
+        provide an end-to-end solution so you can relax. Here&apos;s how I make
+        the process seamless and professional.
+      </p>
+      <p class="w-[75%] mx-auto relative mt-12 font-medium md:text-base sm:text-sm xs:text-xs text-2xl text-justify">
+        Here&apos;s how I make the process seamless and professional:
+      </p>
+
+      <div className="w-[75%] mx-auto relative mt-12" ref={ref}>
+        <motion.div
+          className="absolute left-8 top-0 w-[4px] h-full bg-[#f5f5f5] origin-top md:w-[2px] md:left-[25px] xs:left-[15px]"
+          style={{ scaleY: scrollYProgress }}
+        />
+
+        <ul className="w-full flex flex-col items-start justify-start gap-10 pl-12 xs:pl-8">
+          <Details
+            type="Document Collection"
+            info="I will gather and organize all your paperwork to ensure nothing is missed."
+          />
+          <Details
+            type="Tax Preparation"
+            info="Your tax return is prepared with care and reviewed to ensure it's complete and accurate."
+          />
+          <Details
+            type="Thorough Review"
+            info="Every return is carefully examined to ensure compliance, identify potential tax savings, and maximize your refund."
+          />
+          <Details
+            type="Client Approval"
+            info="Once finalized, the return is shared with you for feedback and approval."
+          />
+          <Details
+            type="Signature Coordination"
+            info="I'll get client’s signatures on all necessary Federal and State signature pages for our records."
+          />
+          <Details
+            type="E-Filing"
+            info="Approved returns will be submitted electronically to the IRS and State authorities."
+          />
+          <Details
+            type="Filing Status Monitoring"
+            info="I'll track the e-file status to ensure everything goes smoothly and address any issues."
+          />
+          <Details
+            type="Re-Filing Support"
+            info="In case of a rejection, I'll promptly resolve the issue and re-file the return."
+          />
+        </ul>
+      </div>
+    </div>
+  );
+}
+
+export default HomePageComp;
--- src/components/Skills.js
+++ src/components/Skills.js
@@ -4,7 +4,7 @@ import { motion } from "framer-motion";
 const Skill = ({ name, x, y }) => {
   return (
     <motion.div
-      className="flex items-center justify-center rounded-full font-semibold bg-[#faf9f6] text-dark py-3 px-6 shadow-lg shadow-[#fff5ee]cursor-pointer absolute lg:py-2 lg:px-4 md:text-sm md:py-1.5 md:px-3 xs:bg-transparent xs:font-bold xs:text-[#f5f5f5]"
+      className="flex items-center justify-center rounded-full font-bold bg-[#faf9f6] text-dark py-3 px-6 shadow-lg shadow-[#fff5ee]cursor-pointer text-xl absolute lg:py-2 lg:px-4 md:text-sm md:py-1.5 md:px-3 xs:bg-transparent xs:font-bold xs:text-[#f5f5f5]"
       whileHover={{ scale: 1.05 }}
       initial={{ x: 0, y: 0 }}
       whileInView={{ x: x, y: y }}
@@ -23,22 +23,22 @@ function Skills() {
       </h2>
       <div className="w-full h-screen relative flex items-center justify-center rounded-full bg-circularLight lg:h-[80vh] sm:h-[60vh] xs:h-[50vh] lg:!bg-circularLightLg md:!bg-circularDarkMg sm:!bg-circularDarkSm">
         <motion.div
-          className="flex items-center justify-center rounded-full font-semibold bg-[#faf9f6] text-dark p-8 shadow-lg shadow-[#fff5ee]cursor-pointer lg:p-6 md:p-4 xs:text-xs xs:p-2"
+          className="flex items-center justify-center rounded-full font-bold text-xl bg-[#faf9f6] text-dark p-8 shadow-lg shadow-[#fff5ee]cursor-pointer lg:p-6 md:p-4 xs:text-xs xs:p-2"
           whileHover={{ scale: 1.05 }}
         >
-          Web
+          Tax Zone
         </motion.div>
 
-        <Skill name="CSS" x="-5vw" y="-10vw" />
-        <Skill name="HTML" x="-25vw" y="2vw" />
-        <Skill name="JS" x="20vw" y="6vw" />
-        <Skill name="ReactJS" x="0vw" y="12vw" />
-        <Skill name="CSS" x="-20vw" y="-15vw" />
-        <Skill name="CSS" x="15vw" y="-12vw" />
-        <Skill name="CSS" x="32vw" y="-5vw" />
-        <Skill name="CSS" x="0vw" y="-20vw" />
-        <Skill name="CSS" x="-25vw" y="18vw" />
-        <Skill name="CSS" x="18vw" y="18vw" />
+        <Skill name="1040" x="-5vw" y="-10vw" />
+        <Skill name="1120" x="-25vw" y="2vw" />
+        <Skill name="1120 - S" x="20vw" y="6vw" />
+        <Skill name="1065" x="0vw" y="12vw" />
+        <Skill name="FinCEN" x="-20vw" y="-15vw" />
+        <Skill name="990" x="15vw" y="-12vw" />
+        <Skill name="5471" x="32vw" y="-5vw" />
+        <Skill name="5472" x="0vw" y="-20vw" />
+        <Skill name="1099s" x="-25vw" y="18vw" />
+        <Skill name="6765" x="18vw" y="18vw" />
       </div>
     </>
   );
