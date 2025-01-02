810db3b80fa99cb573e321a0176ef54930a79dac
Saumya Rawal <114496132+technoprogramming92@users.noreply.github.com>
2025-01-02 13:02:17 +0530

    hireme commented

--- src/components/HireMe.js
+++ src/components/HireMe.js
@@ -6,7 +6,7 @@ function HireMe() {
   return (
     <div className="fixed left-4 bottom-4 flex items-center justify-center md:right-8 md:left-auto md:top-0 md:bottom-auto md:absolute sm:!right-0">
       <div className="w-48 h-auto flex items-center justify-center relative md:w-24">
-        <CircularText className={"fill-dark animate-spin-slow"} />
+        {/* <CircularText className={"fill-dark animate-spin-slow"} /> */}
 
         <Link
           href="mailto:yashshahtaxfreelancer@gmail.com"
