bd8240127995d90cd201bc32f185a87305c20369
Saumya Rawal <114496132+technoprogramming92@users.noreply.github.com>
2025-01-20 23:25:04 +0530

    font hero chaged

--- src/pages/index.js
+++ src/pages/index.js
@@ -45,7 +45,7 @@ export default function Home() {
             <div className="w-1/2 flex flex-col items-center self-center lg:w-full lg:text-center">
               <AnimatedText
                 text="Licensed Enrolled Agent"
-                className="!text-5xl !text-left !text-[#f5f5f5] xl:!text-5xl lg:!text-center lg:!text-6xl md:!text-5xl sm:!text-3xl"
+                className="!text-6xl !text-left !text-[#f5f5f5] xl:!text-5xl lg:!text-center lg:!text-6xl md:!text-5xl sm:!text-3xl"
               />
               <AnimatedText
                 text="Tax Strategist"
